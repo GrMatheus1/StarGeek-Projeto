6421a226faedeb1a992ab0390b9ed4c78a1d3f58
Matheus01~ <matheusbarbosa2827@gmail.com>
2023-05-16 07:57:37 -0300

    lógica inicial

---
+++ login.js
@@ -0,0 +1,40 @@
+const nome = document.getElementById("nome");
+const email = document.getElementById("email");
+const senha = document.getElementById("senha");
+
+formulario.onsubmit = (evento) =>{
+    if(nome.value == ""){
+        evento.preventDefault();
+        mensagem.innerHTML ="<p> Digite Seu Nome! </p>";
+        nome.focus();
+        return null;
+    }
+
+    if(email.value == ""){
+        evento.preventDefault();
+        mensagem.innerHTML = "<p> Digite seu email! </p>";
+        email.focus();
+        return null;
+    }
+
+    if(senha.value == ""){
+        evento.preventDefault();
+        mensagem.innerHTML = "<p> Digite sua senha! </p>";
+        senha.focus();
+        return null;
+    }
+    
+    let dados = JSON.parse(localStorage.getItem("dados")) || [];
+    dados.push({
+        nome : nome.value,
+        email : email.value,
+        senha : senha.value
+    })
+    localStorage.setItem("dados", JSON.stringify(dados));  
+    evento.preventDefault();
+    mensagem.innerHTML="<p> Parabens Cadastro Feito com sucesso! </p>";
+
+    setTimeout(()=>{
+        window.location.assign("login.html");
+    }, 3000);
+}
